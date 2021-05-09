5ae1dfd63bd078edf77856a234af94c5d777d2db
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-09 22:40:45 +0530

    Added: User needs to enter valid first name

--- src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
+++ src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
@@ -1,7 +1,26 @@
 package com.userRegistrationSystem;
 
+import java.util.Scanner;
+
 public class UserRegistrationValidation {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static boolean validateFirstName(String name) {
+        String expression = "^[A-Z]{1}[a-z,A-Z]{2,}";
+        if (name.matches(expression)) {
+            System.out.println("'" + name + "'" + " validation success !");
+            return true;
+        } else {
+            System.out.println("'" + name + "'" + " validation unsuccessful ! ");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
+        System.out.println("Enter First name ");
+        String first_name = scan.nextLine();
+        System.out.println(validateFirstName(first_name));
     }
 }
