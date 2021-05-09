9eb232809e7af4646d700b3d959a06f2adf7464f
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-09 22:19:40 +0530

    Added: Welcome to User registration Program

---
+++ src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
@@ -0,0 +1,7 @@
+package com.userRegistrationSystem;
+
+public class UserRegistrationValidation {
+    public static void main(String[] args) {
+        System.out.println("Welcome to User Registration Project ");
+    }
+}
