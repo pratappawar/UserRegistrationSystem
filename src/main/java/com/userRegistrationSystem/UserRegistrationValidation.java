274b97bbfd28ed6736660b371502bbdcbfe0b098
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-09 23:05:29 +0530

    Added: User needs to enter valid last name

--- src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
+++ src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
@@ -17,10 +17,24 @@ public class UserRegistrationValidation {
         }
     }
 
+    public static boolean validateLastName(String name) {
+        String expression = "^[A-Z]{1}[a-z,A-Z]{2,}";
+        if (name.matches(expression)) {
+            System.out.println("'" + name + "'" + " validation success !");
+            return true;
+        } else {
+            System.out.println("'" + name + "'" + " validation unsuccessful ! ");
+            return false;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
         System.out.println("Enter First name ");
         String first_name = scan.nextLine();
         System.out.println(validateFirstName(first_name));
+        System.out.println("Enter Last name ");
+        String last_name=scan.nextLine();
+        System.out.println(validateLastName(last_name));
     }
 }
