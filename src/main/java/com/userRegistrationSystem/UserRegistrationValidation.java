583ad9e7e6d2a0cd14af13374e7f2a8a4978afc6
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-10 07:55:00 +0530

    Added:  As a User enter Valid email

--- src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
+++ src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
@@ -11,28 +11,44 @@ public class UserRegistrationValidation {
         System.out.println("Enter First name ");
         String first_name = scan.nextLine();
         String firstNamePattern = "^[A-Z]{1}[a-z,A-Z]{2,}";
-        Pattern pattern= Pattern.compile(firstNamePattern);
+        Pattern pattern = Pattern.compile(firstNamePattern);
+
         if ((first_name.matches(firstNamePattern))) {
             System.out.println("'" + first_name + "'" + " validation success !");
         } else {
             System.out.println("'" + first_name + "'" + " validation unsuccessful ! ");
         }
     }
+
     public static void validateLastName() {
         System.out.println("Enter last name ");
         String last_name = scan.nextLine();
         String lastNamePattern = "^[A-Z]{1}[a-z,A-Z]{2,}";
-        Pattern pattern= Pattern.compile(lastNamePattern);
-        if (last_name .matches(lastNamePattern)) {
-            System.out.println("'" + last_name  + "'" + " validation success !");
+        Pattern pattern = Pattern.compile(lastNamePattern);
+        if (last_name.matches(lastNamePattern)) {
+            System.out.println("'" + last_name + "'" + " validation success !");
         } else {
-            System.out.println("'" + last_name  + "'" + " validation unsuccessful ! ");
+            System.out.println("'" + last_name + "'" + " validation unsuccessful ! ");
         }
     }
+
+    public static void validateEmail() {
+        System.out.println("Enter email ");
+        String email = scan.nextLine();         //Accept Email id
+        String emailPattern = "^[a-zA-Z0-9]+[.(a-zA-Z0-9)]*[@]{1}[a-z]+[.]{1}[a-z]{2,4}[.]*[a-z]*{2}$";  //set pattern for email id
+        Pattern pattern = Pattern.compile(emailPattern);
+        if (email.matches(emailPattern)) {                                                               //check Input and Pattern
+            System.out.println("'" + email + "'" + " validation success !");
+        } else {
+            System.out.println("'" + email + "'" + " validation success !");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
         validateFirstName();
         validateLastName();
+        validateEmail();
 
     }
 }
