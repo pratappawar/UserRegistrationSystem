fea51b76ebd3bac95ce6fbd40439ab4e5f515316
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-10 06:53:28 +0530

    Refactor:  method call added

--- src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
+++ src/main/java/com/userRegistrationSystem/UserRegistrationValidation.java
@@ -1,26 +1,27 @@
 package com.userRegistrationSystem;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class UserRegistrationValidation {
 
     static Scanner scan = new Scanner(System.in);
 
-    public static boolean validateFirstName(String name) {
-        String expression = "^[A-Z]{1}[a-z,A-Z]{2,}";
-        if (name.matches(expression)) {
-            System.out.println("'" + name + "'" + " validation success !");
-            return true;
+    public static void validateFirstName() {
+        System.out.println("Enter First name ");
+        String first_name = scan.nextLine();
+        String firstNamePattern = "^[A-Z]{1}[a-z,A-Z]{2,}";
+        Pattern pattern= Pattern.compile(firstNamePattern);
+        if ((first_name.matches(firstNamePattern))) {
+            System.out.println("'" + first_name + "'" + " validation success !");
         } else {
-            System.out.println("'" + name + "'" + " validation unsuccessful ! ");
-            return false;
+            System.out.println("'" + first_name + "'" + " validation unsuccessful ! ");
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Project ");
-        System.out.println("Enter First name ");
-        String first_name = scan.nextLine();
-        System.out.println(validateFirstName(first_name));
+        validateFirstName();
+
     }
 }
